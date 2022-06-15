4be7828435be6557af9cc23e245ddd1331e8b8de
John Ashley Capistrano <82935030+CapistranoJA@users.noreply.github.com>
2022-06-15 23:52:46 +0800

    Migrations/Models

---
+++ database/migrations/2022_06_15_145745_create_applications_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('middlename');
+            $table->string('gender');
+            $table->longText('address');
+            $table->date('birthdate');
+            $table->string('contact_number');
+            $table->string('civilstatus');
+            $table->string('email');
+            $table->string('educattain');
+            $table->string('resume');
+            $table->string('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+};
---
+++ resources/views/pages/adminhome.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.adminlayout')
+@section('content')
+<div class="container pt-5">
+    <div class="row row-cols-1 row-cols-md-4 g-5">
+        <div class="col">
+            <a class ="text-decoration-none" href ="#">
+            <div class="card text-white bg-dark my-3 pt-5 h-100 btn btn-dark" style="max-width: 18rem;" href="#">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Manage Departments</h5>
+                </div>
+              </div>
+            </a>
+        </div>
+        <div class="col">
+            <a class ="text-decoration-none" href ="#">
+            <div class="card text-white bg-dark my-3 pt-5 h-100 btn btn-dark" style="max-width: 18rem;" href="#">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Manage Vacancies</h5>
+                 
+                </div>
+              </div>
+            </a>
+        </div>
+        <div class="col">
+            <a class ="text-decoration-none" href ="#">
+            <div class="card text-white bg-dark my-3 pt-5 h-100 btn btn-dark" style="max-width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Job Applications</h5>
+                </div>
+              </div>
+            </a>
+        </div>
+        <div class="col">
+            <a class ="text-decoration-none" href ="#">
+            <div class="card text-white bg-dark my-3 pt-5 h-100 btn btn-dark" style="max-width: 18rem;" href="#">
+                <div class="card-body"> 
+                  <h5 class="card-title text-center">NUL Employees</h5>
+                </div>
+              </div>
+            </a>
+        </div>
+        <div class="col">
+            <a class ="text-decoration-none" href ="#">
+            <div class="card text-white bg-dark my-3 pt-5 h-100 btn btn-dark" style="max-width: 18rem;" href="#">
+                <div class="card-body"> 
+                  <h5 class="card-title text-center">Manage Admin Accounts</h5>
+                </div>
+              </div>
+            </a>
+        </div>
+      </div>
+</div>
+@endsection
