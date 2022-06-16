fe0634a133b316e3a660f824c1d3783b5448fedf
John Ashley Capistrano <82935030+CapistranoJA@users.noreply.github.com>
2022-06-16 22:13:37 +0800

    blades

--- resources/views/index/layouts/layout.blade.php
+++ resources/views/index/layouts/layout.blade.php
@@ -13,6 +13,7 @@
    </header>
    <div id="main" class="min-vh-100" style="height:100px;background-color:rgb(22,22,22);">
         @include('index._partials._search')
+        @include('index._partials._subhero')
             @yield('content')
    </div>
    <footer>
--- resources/views/index/pages/index.blade.php
+++ resources/views/index/pages/index.blade.php
@@ -1,9 +1,8 @@
 @extends('index.layouts.layout')
 @section('content')
-@include('index._partials._subhero')
 
 <div class="container rounded-3 border border-secondary p-5">
-    @unless()
+
 </div>
 
 
